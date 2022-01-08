a6975e7f6434691c835ef8f9021b600ce2ca85b9
Xander Stoffels <xander.s@outlook.com>
2022-01-08 16:11:55 +0100

    Minecraft version attribute and logo update

---
+++ Beacon.Server/MincraftVersionAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Beacon.Server
+{
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = false)]
+    internal class MincraftVersionAttribute : Attribute
+    {
+        public Version MinecraftVersion { get; }
+        public MincraftVersionAttribute(string mcVersion)
+        {
+            this.MinecraftVersion = Version.Parse(mcVersion.Replace("\"", ""));
+        }
+    }
+}
