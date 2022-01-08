e0bd0986ce0838e8e04e0b868635d586c3af4507
Xander Stoffels <xander.s@outlook.com>
2022-01-08 13:23:28 +0100

    Configuration loading

---
+++ Beacon.Server/Config/ServerConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Beacon.Server.Config
+{
+    internal class ServerConfiguration
+    {
+        public int Port { get; set; }
+
+        public static ServerConfiguration Default => new()
+        {
+            Port = 25565
+        };
+    }
+}
